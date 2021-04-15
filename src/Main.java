6e77c5923ca7742a37bcc7288c3e007863554748
Vlad Korchagin <vkorchagin007@gmail.com>
2021-04-15 17:32:01 +0300

    refactoring

--- src/Main.java
+++ src/Main.java
@@ -1,71 +1,59 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
 
+    private static StringBuilder stringBuilder = new StringBuilder();
+
     public static void main(String[] args) {
 
-        StringBuilder stringBuilder = new StringBuilder();
+        ArrayList<String> dirList = new ArrayList<>();
+        dirList.add("H:\\Games\\src");
+        dirList.add("H:\\Games\\res");
+        dirList.add("H:\\Games\\saveGames");
+        dirList.add("H:\\Games\\temp");
+        dirList.add("H:\\Games\\src\\main");
+        dirList.add("H:\\Games\\src\\test");
+        dirList.add("H:\\Games\\res\\drawables");
+        dirList.add("H:\\Games\\res\\vectors");
+        dirList.add("H:\\Games\\res\\icons");
 
-        File src = new File("H:\\Games\\src");
-        if (src.mkdir())
-            stringBuilder.append("Каталог " + src.getName() + " создан\n");
-        File res = new File("H:\\Games\\res");
-        if (res.mkdir())
-            stringBuilder.append("Каталог " + res.getName() + " создан\n");
-        File saveGames = new File("H:\\Games\\saveGames");
-        if (saveGames.mkdir())
-            stringBuilder.append("Каталог " + saveGames.getName() + " создан\n");
-        File temp = new File("H:\\Games\\temp");
-        if (temp.mkdir())
-            stringBuilder.append("Каталог " + temp.getName() + " создан\n");
-        File main = new File("H:\\Games\\src\\main");
-        if (main.mkdir())
-            stringBuilder.append("Каталог " + main.getName() + " создан\n");
-        File test = new File("H:\\Games\\src\\test");
-        if (test.mkdir())
-            stringBuilder.append("Каталог " + test.getName() + " создан\n");
-        File drawables = new File("H:\\Games\\res\\drawables");
-        if (drawables.mkdir())
-            stringBuilder.append("Каталог " + drawables.getName() + " создан\n");
-        File vectors = new File("H:\\Games\\res\\vectors");
-        if (vectors.mkdir())
-            stringBuilder.append("Каталог " + vectors.getName() + " создан\n");
-        File icons = new File("H:\\Games\\res\\icons");
-        if (icons.mkdir())
-            stringBuilder.append("Каталог " + icons.getName() + " создан\n");
+        ArrayList<String> filesList = new ArrayList<>();
+        filesList.add("H:\\Games\\src\\main//Main.java");
+        filesList.add("H:\\Games\\src\\main//Utils.java");
+        filesList.add("H:\\Games\\temp//temp.txt");
 
-        File mainJava = new File("H:\\Games\\src\\main//Main.java");
-        try {
-            if (mainJava.createNewFile())
-                stringBuilder.append("Файл " + mainJava.getName() + " успешно создан\n");
-        }   catch (IOException ex) {
-            stringBuilder.append("Ошибка при создании файла\n");
-        }
-
-        File utilsJava = new File("H:\\Games\\src\\main//Utils.java");
-        try {
-            if (utilsJava.createNewFile())
-                stringBuilder.append("Файл " + utilsJava.getName() + " успешно создан\n");
-        }   catch (IOException ex) {
-            stringBuilder.append("Ошибка при создании файла\n");
-        }
-
-        File tempTxt = new File("H:\\Games\\temp//temp.txt");
-        try {
-            if (tempTxt.createNewFile())
-                stringBuilder.append("Файл " + tempTxt.getName() + " успешно создан\n");
-        }   catch (IOException ex) {
-            stringBuilder.append("Ошибка при создании файла\n");
-        }
+        createDir(dirList);
+        createFiles(filesList);
 
         //System.out.println(stringBuilder.toString());
 
-        try (FileWriter fileWriter = new FileWriter(tempTxt, false)) {
+        try (FileWriter fileWriter = new FileWriter("H:\\Games\\temp//temp.txt", false)) {
             fileWriter.write(stringBuilder.toString());
         } catch (IOException ex) {
-            System.out.println("Ошибка при записи файла temp.txt");
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public static void createDir (ArrayList<String> arrayList) {
+        for (String path : arrayList){
+            File src = new File(path);
+            if (src.mkdir())
+                stringBuilder.append("Каталог " + src.getName() + " создан\n");
+        }
+    }
+
+    public static void createFiles (ArrayList<String> arrayList) {
+        for (String file : arrayList) {
+            File newFile = new File(file);
+            try {
+                if (newFile.createNewFile())
+                    stringBuilder.append("Файл " + newFile.getName() + " успешно создан\n");
+            }   catch (IOException ex) {
+                stringBuilder.append("Ошибка при создании файла\n");
+            }
         }
     }
 }
